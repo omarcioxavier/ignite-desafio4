9aaac948336c64ac8e3c95ee3d87eb00e5d0067d
Márcio Xavier <juniorxavier606@gmail.com>
2022-12-26 20:14:16 -0300

    Ajustes iniciais

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import GlobalStyle from './styles/global';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <AppRoutes />
+      </Router>
+    </>);
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from '../pages/Dashboard';
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" exact-component={Dashboard} />
+    </Routes>
+  );
+};
